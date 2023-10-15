16e13c2dc289e5f8a5faf4f784a0ede3db4e49e2
othmenbelgacem <othmanbelgacem8@gmail.com>
2023-10-15 16:26:22 +0100

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -8,7 +8,7 @@ namespace PartsUnlimited.Models
     {
         [Key]
         public int CartItemId { get; set; }
-
+//my second change
         [Required]
         public string CartId { get; set; }
         public int ProductId { get; set; }
